101fcd83a43607b9bf647a8ba7585b8970d70d32
DAVID\david <davidalejangp@gmail.com>
2024-11-25 15:52:01 -0500

    COnfiguracion para android y Creacion de pantallas

---
+++ src/screens/RestuarantsScreen.js
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function RestuarantsScreen() {
+  return (
+    <View>
+      <Text>RestuarantsScreen</Text>
+    </View>
+  );
+}
